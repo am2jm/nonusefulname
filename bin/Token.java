01901f31c4e6f81f418e59f2283fa4823425f2b8
Divya354 <dsk5df@virginia.edu>
2017-02-13 22:59:11 -0500

    all project files in bin

---
+++ bin/Token.java
@@ -0,0 +1,28 @@
+// Token.java
+
+/**
+ * Store values associated with a token
+ */
+
+public class Token {
+    private Object lexeme;
+    private tok tokType;
+    private int line, col;
+
+    public Token(tok type, int line, int col, Object value) {
+        this.tokType = type;
+        this.line = line;
+        this.col = col;
+        this.lexeme = value;
+    }
+
+    public String toString() {
+        if(this.lexeme == null) {
+            return this.line + "\n"+
+                         this.tokType;
+        } else {
+            return this.line + "\n" +
+                         this.tokType + "\n" + this.lexeme;
+        }
+    }
+}
---
+++ bin/tok.java
@@ -0,0 +1,54 @@
+public enum tok {
+    AT("at"),
+    CASE("case"),
+    CLASS("class"),
+    COLON("colon"),
+    COMMA("comma"),
+    DIVIDE("divide"),
+    DOT("dot"),
+    ELSE("else"),
+    EQUALS("equals"),
+    ESAC("esac"),
+    FALSE("false"),
+    FI("fi"),
+    IDENT("identifier"),
+    IF("if"),
+    IN("in"),
+    INHERITS("inherits"),
+    INT("integer"),
+    ISVOID("isvoid"),
+    LARROW("larrow"),
+    LBRACE("lbrace"),
+    LE("le"),
+    LET("let"),
+    LOOP("loop"),
+    LPAREN("lparen"),
+    LT("lt"),
+    MINUS("minus"),
+    NEW("new"),
+    NOT("not"),
+    OF("of"),
+    PLUS("plus"),
+    POOL("pool"),
+    RARROW("rarrow"),
+    RBRACE("rbrace"),
+    RPAREN("rparen"),
+    SEMI("semi"),
+    STRING("string"),
+    THEN("then"),
+    TILDE("tilde"),
+    TIMES("times"),
+    TRUE("true"),
+    TYPE("type"),
+    WHILE("while");
+
+    private final String name;
+
+    private tok(String s) {
+        this.name = s;
+    }
+
+    public String toString() {
+        return name;
+    }
+}
